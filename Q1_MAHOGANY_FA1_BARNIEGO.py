835acb1574bfc581846c849292a0093717a50613
ALEXISAURA <acabarniego31@src.pshs.edu.ph>
2026-08-13 09:42:26 +0800

    Add files via upload

---
+++ Q1_MAHOGANY_FA1_BARNIEGO.py
@@ -0,0 +1,23 @@
+import math
+
+Value_of_x1 = float(input("Enter the value of x1: "))
+Value_of_x2 = float(input("Enter the value of x2: "))
+Value_of_y1 = float(input("Enter the value of y1: "))
+Value_of_y2 = float(input("Enter the value of y2: "))
+
+Point1 = (Value_of_x1, Value_of_y1)
+Point2 = (Value_of_x2, Value_of_y2)
+
+Distance = math.sqrt(math.pow(Value_of_x2 - Value_of_x1, 2) + math.pow(Value_of_y2 - Value_of_y1, 2))
+print("The distance between the two points is:", Distance)
+
+# guide questions
+"""
+# It made it easier and helped me learn. It also guided us to complete our program and be better.
+"""
+"""
+# The distance code was easy to put on because it was automatic but aside from that we learned how to use it. It made us finish our program faster and it was a great help.
+"""
+"""
+# We would go into a lot of process and would take us longer. We would have never been able to complete and finish the program.
+"""
